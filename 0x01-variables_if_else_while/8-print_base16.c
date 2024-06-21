7f974b52cfbad35c67942bbc256aee4f280d69d2
Emmanuel Quayson <kalmeqwhame@outlook.com>
2024-06-21 16:49:44 -0700

    Complete all mandatory tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lower case,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (num = 0; num < 6; num++)
+	{
+		putchar(num + 'a')
+	}
+	putchar('\n');
+
+	return (0);
+}
