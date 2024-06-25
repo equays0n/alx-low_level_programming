3868dc90771d8564a05dff517df6b66d87e11c2d
Emmanuel Quayson <kalmeqwhame@outlook.com>
2024-06-25 08:25:46 -0700

    Rename files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - prints _putchar followed by a new line
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	return write(1, &c, 1);
+	char *str = "_putchar";
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+	return  (0);
 }
