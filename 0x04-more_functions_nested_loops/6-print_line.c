2c45bed62872eecc41724fd539aaa8047aa63dcf
Emmanuel Quayson <kalmeqwhame@outlook.com>
2024-06-27 08:43:07 -0700

    Second half begins

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Print a straight line
+ * followed by a new line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
