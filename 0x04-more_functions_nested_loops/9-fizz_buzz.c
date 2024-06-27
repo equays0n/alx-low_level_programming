912cb585915bcdf6f1e449aec76b353053b45458
Emmanuel Quayson <kalmeqwhame@outlook.com>
2024-06-27 09:21:14 -0700

    Complete mandatory tasks

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - Prints the numbers from 1 to 100
+ * replacing multiples of 3 with "Fizz",
+ * multiples of 5 with "Buzz",
+ * and multiples of both with "FizzBuzz".
+ */
+void fizz_buzz(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
