5c3e149278f3e5e3878b4a8df8af02021bc9fdde
Emmanuel Quayson <kalmeqwhame@outlook.com>
2024-07-08 16:32:37 -0700

    Fix error

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: pointer to the destination memory area
  */
-char *_memcpy(char *dest, const char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
